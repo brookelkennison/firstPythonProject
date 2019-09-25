5ac5c86e9c0acf99fc645d9edd6ec428e3361c95
Brooke Kennison <brookelkennison@gmail.com>
2019-09-24 20:49:45 -0500

    hello world working in local host

--- fistProject/urls.py
+++ fistProject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('', include('collection.urls'))
+
 ]
